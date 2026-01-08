123330b687eb46a337ab146fbaf51165eaa11688
WhichWitchWife <WhichWitchWife@pm.me>
2026-01-08 14:04:22 -0800

    add way to change font

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import Font from "@/Font.vue";
+import "./style.css";
+
+const font = ref("open-dyslexia");
+</script>
+
 <template>
-  <h1 class="header">Role-playing Services Form</h1>
-  <div class="input-form">
+  <div class="app-wrapper" :class="font">
+    <h1 class="header">Role-playing Services Form</h1>
+    <Font v-model="font" />
     <div class="input-grid">
       <label for="starting_amount">Starting Gil</label>
       <input id="starting_amount" type="number" min="1" step="1" />
---
+++ src/Font.vue
@@ -0,0 +1,14 @@
+<script setup>
+const font = defineModel();
+</script>
+
+<template>
+  <div class="font-selector">
+    <label for="font">Font choice</label>
+    <select name="font" v-model="font">
+      <option value="open-dyslexia">Open Dyslexia</option>
+      <option value="times-new-roman">Times New Roman</option>
+      <option value="aevnir">Aevnir</option>
+    </select>
+  </div>
+</template>
