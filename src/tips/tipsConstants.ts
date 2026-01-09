c9fc042a2308ecc2eb26d88710379a08fc9ba2ae
WhichWitchWife <WhichWitchWife@pm.me>
2026-01-08 21:05:04 -0800

    please for the love of god work

--- src/tips/tipsConstants.ts
+++ src/tips/tipsConstants.ts
@@ -1,18 +1,23 @@
+interface Tip {
+  amount: number;
+  timestamp: string;
+}
+
 export const TIP_HISTORY = "tipHistory";
 export function getTipHistory() {
   return JSON.parse(localStorage.getItem(TIP_HISTORY) || "") || [];
 }
 
-export function dangerousSetTipHistory(newTipHistory) {
+export function dangerousSetTipHistory(newTipHistory: string) {
   localStorage.setItem(TIP_HISTORY, newTipHistory);
 }
-export function pushOntoLocalStorageTips(newTip) {
+export function pushOntoLocalStorageTips(newTip: Tip) {
   const tipHistory = getTipHistory();
   const newHistory = [...tipHistory, newTip];
   dangerousSetTipHistory(JSON.stringify(newHistory));
 }
 
-export function deleteIndexInLocalStorageTips(deletedIndex) {
+export function deleteIndexInLocalStorageTips(deletedIndex: number) {
   const history = getTipHistory();
   dangerousSetTipHistory(
     JSON.stringify([
