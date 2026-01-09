ac9e6977aa22fd04bd13c2163ebe1bd34e377868
WhichWitchWife <WhichWitchWife@pm.me>
2026-01-08 18:23:11 -0800

    enable deleting tips

--- src/Tips.vue
+++ src/Tips.vue
@@ -1,12 +1,23 @@
 <script setup>
 import { onMounted, ref } from "vue";
 import TipInput from "@/tips/TipInput.vue";
+import {deleteIndexInLocalStorageTips} from "@/tips/tipsConstants.js";
 
 const tipHistory = ref([]);
 
 const onPushedTip = tip => {
   tipHistory.value = [...tipHistory.value, tip];
 };
+
+const deleteTip = (tip) => {
+  const history = tipHistory.value;
+  const deletedIndex = history.findIndex((t) => t.timestamp === tip.timestamp && t.amount === tip.amount)
+  tipHistory.value = [
+      ...history.slice(0, deletedIndex),
+    ...history.slice(deletedIndex + 1, history.length)
+  ];
+  deleteIndexInLocalStorageTips(deletedIndex);
+}
 
 onMounted(() => {
   tipHistory.value = JSON.parse(localStorage.getItem("tipHistory"));
@@ -19,10 +30,12 @@ onMounted(() => {
     <tr>
       <th>Timestamp</th>
       <th>Tip Amount</th>
+      <th>Tip Actions</th>
     </tr>
     <tr v-for="tip in tipHistory">
       <td>{{ tip.timestamp }}</td>
       <td>{{ tip.amount }}</td>
+      <td><button @click="() => deleteTip(tip)">Delete Tip</button></td>
     </tr>
   </table>
 </template>
--- src/tips/tipsConstants.ts
+++ src/tips/tipsConstants.ts
@@ -9,8 +9,15 @@ export function dangerousSetTipHistory(newTipHistory) {
 export function pushOntoLocalStorageTips(newTip) {
   const tipHistory = getTipHistory();
   const newHistory = [...tipHistory, newTip];
-  console.log(tipHistory);
-  console.log(newTip);
-  console.log(newHistory);
   dangerousSetTipHistory(JSON.stringify(newHistory));
+}
+
+export function deleteIndexInLocalStorageTips(deletedIndex) {
+  const history = getTipHistory();
+  dangerousSetTipHistory(
+    JSON.stringify([
+      ...history.slice(0, deletedIndex),
+      ...history.slice(deletedIndex + 1, history.length),
+    ])
+  );
 }
