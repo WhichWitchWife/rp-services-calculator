b35a85c1631be7191b2c66719f55460e2a2f232e
WhichWitchWife <WhichWitchWife@pm.me>
2026-01-08 17:04:35 -0800

    add more inputs

--- src/Settings.vue
+++ src/Settings.vue
@@ -1,7 +1,7 @@
 <script setup>
 import Font from "@/setting-inputs/Font.vue";
 import NumericalInput from "@/setting-inputs/NumericalInput.vue";
-import {HOUSE_CUT_KEY} from "@/setting-inputs/settingConstants.js";
+import {BIG_TIP_THRESHOLD, HOUSE_CUT, NUMBER_WORKERS_PRESENT} from "@/setting-inputs/settingConstants.js";
 
 defineProps({
   font: String,
@@ -21,9 +21,19 @@ defineEmits([
     <div class="settings">
       <Font @input="(e) => $emit('onFontChange', e.target.value)" :modelValue="font"/>
       <NumericalInput
-          :setting-key="HOUSE_CUT_KEY"
+          :setting-key="HOUSE_CUT"
           setting-name="House Cut (%)"
           :setting-input-options="{ max: 100 }"
+      />
+      <NumericalInput
+          :setting-key="BIG_TIP_THRESHOLD"
+          setting-name="Big Tip Threshold"
+          :setting-input-options="{ min: 1 }"
+      />
+      <NumericalInput
+          :setting-key="NUMBER_WORKERS_PRESENT"
+          setting-name="# of Workers Currently Present"
+          :setting-input-options="{ min: 1 }"
       />
     </div>
     <button class="close" @click="$emit('closeSettings')">Close</button>
--- src/setting-inputs/settingConstants.js
+++ src/setting-inputs/settingConstants.js
@@ -1,1 +1,3 @@
-export const HOUSE_CUT_KEY = 'house-cut';
+export const HOUSE_CUT = 'house-cut';
+export const NUMBER_WORKERS_PRESENT = 'num-workers';
+export const BIG_TIP_THRESHOLD = 'big-tip';
