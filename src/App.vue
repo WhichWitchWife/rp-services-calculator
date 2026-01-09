6a90150f0e615575e229025a7088a355afc38f1e
WhichWitchWife <WhichWitchWife@pm.me>
2026-01-08 19:59:17 -0800

    persist gil values on localStorage

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@ import Settings from "@/Settings.vue";
 import Tips from "@/Tips.vue";
 import Results from "@/Results.vue";
 import { deleteIndexInLocalStorageTips } from "@/tips/tipsConstants.js";
-import "./style.css";
 import {
   getBigTipThreshold,
   getHouseCut,
   getNumberWorkersPresent,
 } from "@/setting-inputs/settingConstants.js";
+import "./style.css";
 
 onMounted(() => {
   font.value = localStorage.getItem("font") || "open-dyslexia";
@@ -19,6 +19,8 @@ onMounted(() => {
     bigTip: getBigTipThreshold(),
   };
   tipHistory.value = JSON.parse(localStorage.getItem("tipHistory"));
+  startingGil.value = Number(localStorage.getItem('startingGil')) || 0;
+  endingGil.value = Number(localStorage.getItem('endingGil')) || 0;
 });
 const settingsOpen = ref(false);
 
@@ -54,6 +56,17 @@ const onFontChange = newFont => {
 
 const startingGil = ref(0);
 const endingGil = ref(0);
+
+const startingGilChange = (val) => {
+  localStorage.setItem('startingGil', val);
+  startingGil.value = Number(val);
+}
+
+const endingGilChange = (val) => {
+  localStorage.setItem('endingGil', val);
+  endingGil.value = Number(val);
+}
+
 </script>
 
 <template>
@@ -74,7 +87,8 @@ const endingGil = ref(0);
         type="number"
         min="1"
         step="1"
-        v-model="startingGil"
+        :value="startingGil"
+        @input="(e) => startingGilChange(e.target.value)"
       />
       <label for="ending_amount">Ending Gil</label>
       <input
@@ -82,7 +96,8 @@ const endingGil = ref(0);
         type="number"
         min="1"
         step="1"
-        v-model="endingGil"
+        :value="endingGil"
+        @input="(e) => endingGilChange(e.target.value)"
       />
     </div>
     <Tips
